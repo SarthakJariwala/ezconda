6e312c2b186148bb2062cace6f9db4495513180c
Sarthak <jariwala@uw.edu>
2021-09-18 10:10:10 -0700

    lockfile generation now using the conda cli python api

--- src/ezconda/experimental/lock.py
+++ src/ezconda/experimental/lock.py
@@ -10,7 +10,6 @@ __all__ = ["write_lock_file"]
 def write_lock_file(env_name):
     typer.secho(f"Writing lockfile...", fg=typer.colors.YELLOW)
     # generate lockfile
-    # o = subprocess.run(["conda", "list", "-n", f"{env_name}", "--json"], stdout=PIPE)
     stdout, stderr, exit_code = run_command(Commands.LIST, "-n", f"{env_name}", "--json")
     with open(f"{env_name}.lockfile", "w") as f:
         json.dump(json.loads(stdout), f, indent=4)
