b29c957352a93d0b9c712757e98f4b360a97c762
Sarthak <jariwala@uw.edu>
2021-09-23 22:22:18 -0700

    add tests for verbose

--- tests/test_create.py
+++ tests/test_create.py
@@ -17,6 +17,16 @@ def test_create_without_install(clean_up_env_after_test):
     assert "Writing specifications to" in result.stdout
 
 
+@pytest.mark. usefixtures("clean_up_env_after_test")
+def test_verbose(clean_up_env_after_test):
+    result = runner.invoke(app, ["create", "-n", "test", "-v"])
+
+    assert Path("test.yml").is_file()
+    assert "Creating new conda environment" in result.stdout
+    # from conda
+    assert "Collecting package metadata (current_repodata.json):" in result.stdout
+
+
 @pytest.mark.usefixtures("clean_up_env_after_test")
 def test_create_w_pkg_install(clean_up_env_after_test):
     result = runner.invoke(app, ["create", "-n", "test", "numpy"])
--- tests/test_install.py
+++ tests/test_install.py
@@ -16,6 +16,15 @@ def test_install_w_no_existing_pkgs(clean_up_env_after_test):
 
 
 @pytest.mark. usefixtures("clean_up_env_after_test")
+def test_verbose_install(clean_up_env_after_test):
+    _ = runner.invoke(app, ["create", "-n", "test"])
+    result = runner.invoke(app, ["install", "-n", "test", "python=3.8", "-v"])
+
+    assert "Installing packages..." in result.stdout
+    assert "Collecting package metadata (current_repodata.json):" in result.stdout
+
+
+@pytest.mark. usefixtures("clean_up_env_after_test")
 def test_install_w_channel_and_no_existing_pkgs(clean_up_env_after_test):
     _ = runner.invoke(app, ["create", "-n", "test"])
     result = runner.invoke(app, ["install", "-n", "test", "-c", "conda-forge", "python=3.8"])
