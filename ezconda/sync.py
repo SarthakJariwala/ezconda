5cd025318109a322041455e549bc2502c1bda0fb
Sarthak Jariwala <jariwala@uw.edu>
2022-06-16 16:08:35 -0700

    allow sync with lockfile; add --with option

--- ezconda/sync.py
+++ ezconda/sync.py
@@ -1,6 +1,10 @@
 import subprocess
 import typer
+import sys
+import platform
 from typing import Optional
+from pathlib import Path
+from enum import Enum
 
 from .console import console
 from ._utils import get_validate_file_name
@@ -8,6 +12,12 @@ from .solver import Solver
 from .config import get_default_solver
 from .summary import get_summary_for_revision
 from .experimental import write_lock_file
+from .recreate import read_lock_file_and_install
+
+
+class SyncFile(str, Enum):
+    specfile = "specfile"
+    lockfile = "lockfile"
 
 
 def sync(
@@ -18,7 +28,13 @@ def sync(
         prompt="Name of the local environment to sync",
         help="Name of the local environment to sync",
     ),
-    file: Optional[str] = typer.Option(
+    sync_with: SyncFile = typer.Option(
+        ...,
+        "--with",
+        prompt=True,
+        help="Sync environment with either lock file or specifications file",
+    ),
+    file: Optional[Path] = typer.Option(
         None,
         "--file",
         "-f",
@@ -34,53 +50,67 @@ def sync(
     lock: Optional[bool] = typer.Option(True, help="Write lockfile"),
 ):
     """
-    Sync local environment with conda-environment 'yml' file.
+    Sync local environment with changes made to environment specifications file ('yml') or lockfile.
 
-    Changes made to the environment yml file will be reflected in the local environment.
+    Syncing local environment with specifications file will update existing lockfile.
     """
 
-    with console.status(f"[magenta]Validating environment and file") as status:
+    if solver is None:
+        solver = get_default_solver()
 
-        file = get_validate_file_name(env_name, file)
-
-        if solver is None:
-            solver = get_default_solver()
-
-        status.update(
-            f"[magenta]Syncing local environment '{env_name}' with file '{file}'"
-        )
-
-        p = subprocess.run(
-            [
-                f"{solver.value}",
-                "env",
-                "update",
-                "-n",
-                env_name,
-                "--file",
-                file,
-                "--prune",
-            ],
-            capture_output=True,
-            text=True,
-        )
-
-        if p.returncode != 0:
-            console.print(f"[red]{str(p.stdout + p.stderr)}")
-            raise typer.Exit()
-
-        if verbose:
-            console.print(f"[yellow]{str(p.stdout)}")
+    if sync_with == SyncFile.lockfile:
+        
+        if file is None:
+            file = Path(f"{env_name}-{sys.platform}-{platform.machine()}.lock")
+        
+        read_lock_file_and_install(file, solver, verbose, env_name)
 
         console.print(
             f"[bold green] :arrows_counterclockwise: '{env_name}' & '{file}' are now in sync!"
         )
 
-        if lock:
-            status.update(f"[magenta]Writing lock file")
-            write_lock_file(env_name)
-
         console.print(f"[bold green] :star: Done!")
 
-        if summary:
-            get_summary_for_revision(env_name)
+    else:
+        with console.status(f"[magenta]Validating environment and file") as status:
+
+            file = get_validate_file_name(env_name, file)
+
+            status.update(
+                f"[magenta]Syncing local environment '{env_name}' with file '{file}'"
+            )
+
+            p = subprocess.run(
+                [
+                    f"{solver.value}",
+                    "env",
+                    "update",
+                    "-n",
+                    env_name,
+                    "--file",
+                    file,
+                    "--prune",
+                ],
+                capture_output=True,
+                text=True,
+            )
+
+            if p.returncode != 0:
+                console.print(f"[red]{str(p.stdout + p.stderr)}")
+                raise typer.Exit()
+
+            if verbose:
+                console.print(f"[yellow]{str(p.stdout)}")
+
+            console.print(
+                f"[bold green] :arrows_counterclockwise: '{env_name}' & '{file}' are now in sync!"
+            )
+
+            if lock:
+                status.update(f"[magenta]Writing lock file")
+                write_lock_file(env_name)
+
+            console.print(f"[bold green] :star: Done!")
+
+            if summary:
+                get_summary_for_revision(env_name)
--- tests/test_sync.py
+++ tests/test_sync.py
@@ -1,4 +1,7 @@
 import pytest
+import shutil
+import sys
+import platform
 from typer.testing import CliRunner
 from ezconda.main import app
 from ezconda._utils import read_env_file, add_pkg_to_dependencies, write_env_file
@@ -9,7 +12,7 @@ runner = CliRunner()
 
 
 @pytest.mark.usefixtures("clean_up_env_after_test")
-def test_env_sync_w_spec_file():
+def test_env_sync_w_specfile():
     # create a new environment
     _ = runner.invoke(app, ["create", "-n", "test", "python=3.9"])
 
@@ -19,12 +22,35 @@ def test_env_sync_w_spec_file():
     write_env_file(env_specs, "test.yml")
 
     # sync the environment
-    result = runner.invoke(app, ["sync", "-n", "test"])
+    result = runner.invoke(app, ["sync", "-n", "test", "--with", "specfile"])
 
     assert result.exit_code == 0
 
     # check if numpy is in the environment
     check_if_pkg_is_installed("test", "numpy")
+
+
+@pytest.mark.usefixtures("clean_up_env_after_test")
+def test_env_sync_w_lockfile():
+    # create a new environment with packages
+    _ = runner.invoke(app, ["create", "-n", "test", "python=3.9", "numpy"])
+
+    # create a new environment with no packages
+    _ = runner.invoke(app, ["create", "-n", "test2"])
+
+    # copy lockfile and rename to test2
+    shutil.copy(
+        f"test-{sys.platform}-{platform.machine()}.lock",
+        f"test2-{sys.platform}-{platform.machine()}.lock"
+    )
+
+    # sync the environment test2 with new test2 lockfile
+    result = runner.invoke(app, ["sync", "-n", "test2", "--with", "lockfile"])
+
+    assert result.exit_code == 0
+
+    # check if numpy is in the test2 environment that was intially empty
+    check_if_pkg_is_installed("test2", "numpy")
 
 
 @pytest.mark.usefixtures("clean_up_env_after_test")
@@ -37,7 +63,7 @@ def test_verbose_install_w_conda(clean_up_env_after_test):
     write_env_file(env_specs, "test.yml")
 
     result = runner.invoke(
-        app, ["sync", "-n", "test", "-v", "--solver", "conda"]
+        app, ["sync", "-n", "test", "--with", "specfile", "-v", "--solver", "conda"]
     )
 
     assert result.exit_code == 0
