be10639cb6182c98142bf880a54fa4d0638f73ae
Sarthak Jariwala <jariwala@uw.edu>
2022-06-15 19:50:51 -0700

    fix version number

--- ezconda/__init__.py
+++ ezconda/__init__.py
@@ -1,1 +1,7 @@
-__version__ = "0.5.0"
+try:
+    from importlib.metadata import version  # type: ignore
+except ImportError:  # pragma: no cover
+    from importlib_metadata import version  # type: ignore
+
+
+__version__ = version(__name__)
