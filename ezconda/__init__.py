a0963fca8683cfdfde6e719997f5488cef9ee8e4
Sarthak Jariwala <jariwala@uw.edu>
2022-06-15 19:58:13 -0700

    remove unnecessary imports

--- ezconda/__init__.py
+++ ezconda/__init__.py
@@ -1,7 +1,4 @@
-try:
-    from importlib.metadata import version  # type: ignore
-except ImportError:  # pragma: no cover
-    from importlib_metadata import version  # type: ignore
+from importlib_metadata import version  # type: ignore
 
 
 __version__ = version(__name__)
