a17798f88eb3807c475e1568a4def949cc2e18f8
Sarthak <jariwala@uw.edu>
2022-07-01 14:33:27 -0700

    Remove command internal refactor to use mamba repoquery whoneeds

--- ezconda/remove.py
+++ ezconda/remove.py
@@ -1,10 +1,11 @@
-import subprocess
 import typer
 import json
 import os
 import conda.exports
 
 from typing import List, Optional
+
+from rich.tree import Tree
 
 from .console import console
 from ._utils import (
@@ -51,34 +52,41 @@ def remove(
 
     with console.status(f"[magenta]Validating file, packages, channels") as status:
         file = get_validate_file_name(env_name, file)
+        env_specs = read_env_file(file)
+        channels = env_specs["channels"]
 
         installed_packages = [
             specs["name"]
             for specs in json.load(os.popen(f"conda list -n {env_name} --json"))
         ]
-        root_prefix = json.load(os.popen(f"conda info -e --json"))["root_prefix"]
-        linked_data = conda.exports.linked_data(root_prefix)
+        
+        for pkg in pkg_name:
+            cmd = ["mamba", "repoquery", "whoneeds"]
+            for chn in channels:
+                cmd.append("-c")
+                cmd.append(chn)
+            cmd.append(f"{pkg}")
+            cmd.append("--json")
 
-        other_pkg_that_depends_on_pkg = []
-        for k in linked_data.keys():
-            if linked_data[k]["name"] in installed_packages:
-                # check the dependencies
-                # if the package listed for removal is listed as dependency for any other installed package
-                # let the user know before they confirm to the removal
-                for deps in linked_data[k]["depends"]:
-                    for pkg in pkg_name:  # unpack multiple packages
-                        if pkg in deps.split(" "):
-                            other_pkg_that_depends_on_pkg.append(linked_data[k]["name"])
+            output = run_command(cmd, verbose=False)
+            formatted_output = json.loads(output.stdout)
+            dependent_pkgs_info = formatted_output["result"]["pkgs"]
 
-        if other_pkg_that_depends_on_pkg:
-            console.print(f"[magenta]There are packages that depend on {pkg_name}")
-            console.print(
-                f"[magenta]Removing {pkg_name} will also remove the following:"
-            )
-            console.print(f"[magenta]{other_pkg_that_depends_on_pkg}\n")
-            status.stop()
-            typer.confirm(f"Do you want to continue?", abort=True)
-            status.start()
+            if dependent_pkgs_info:
+                dependent_pkgs = [p['name'] for p in dependent_pkgs_info]
+                # check if any of the installed packages require this as dep
+                intersection_pkgs = set(dependent_pkgs).intersection(set(installed_packages))
+                
+                if intersection_pkgs:
+                    tree = Tree(f"[bold yellow]{pkg} is required by[/]")
+                    console.print(f"[bold magenta] :warning: There are packages that depend on {pkg}\n")
+                    console.print(f"[bold magenta] :warning: Removing {pkg} will also remove them!\n")
+                    for dep_pk in intersection_pkgs:
+                        tree.add(f"[bold yellow]{dep_pk}[/]")
+                    console.print(tree)
+                    status.stop()
+                    typer.confirm(f"Do you want to continue?", abort=True)
+                    status.start()
 
         env_specs = read_env_file(file)
         env_specs = remove_pkg_from_dependencies(env_specs, pkg_name)
@@ -105,7 +113,7 @@ def remove(
         # if so, remove them from .yml file
         env_specs = recheck_dependencies(env_specs, env_name)
 
-        console.print(f"[bold green] :rocket: Removed packages from {env_name}")
+        console.print(f"[bold green] :cross_mark_button: Removed packages from {env_name}")
 
         status.update(f"[magenta]Writing specifications to {file}")
         write_env_file(env_specs, file)
